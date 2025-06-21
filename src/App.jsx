648538855e44311841287c5f7368781eb8bd9690
Your Name <abiarastephen412@gmail.com>
2025-06-21 10:37:07 +0100

    user analyist

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -29,6 +30,7 @@ function App() {
         <Route path="/otp" element={<OtpComponent />} />
       </Routes>
       <ToastContainer />
+      <Analytics />
     </Router>
   );
 }
