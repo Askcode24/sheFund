aa0a189a9789738896585eb85cb9a26c3c0aad5b
Your Name <abiarastephen412@gmail.com>
2025-08-24 01:25:47 +0100

    fix:correct casing of Registers image import path in forgetpassword component

--- src/component/ForgetPassword.jsx
+++ src/component/ForgetPassword.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
-import EmailImg from '../images/registers/image1.png';
-import PasswordImg from '../images/registers/image2.png';
+import EmailImg from '../images/Registers/image1.png';
+import PasswordImg from '../images/Registers/image2.png';
 import Logo from '../images/Logo/logo/Dark.png';
 import AuthLayout from '../component/AuthLayout';
 import { useState } from 'react';
