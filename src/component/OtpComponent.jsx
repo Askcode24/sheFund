620000a6274ed8587607ce3d03f0d4f38c731f7d
Your Name <abiarastephen412@gmail.com>
2025-08-28 16:49:06 +0100

    fix: update image import path by using require method

--- src/component/OtpComponent.jsx
+++ src/component/OtpComponent.jsx
@@ -1,7 +1,6 @@
 import OTPInput from 'otp-input-react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import Logo from '../images/logo/darklogo/dark.png';
 import { useState, useEffect } from 'react';
 import AuthLayout from '../component/AuthLayout';
 import AuthService from '../api/authService';
